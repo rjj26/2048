5cf3d0cd39354e9b68846cf3fea9199f0499dc80
VinhTran <vinhtrann101@gmail.com>
2023-12-10 23:59:53 -0500

    tile statistics

--- test.py
+++ test.py
@@ -7,8 +7,27 @@ from greedy import greedy_policy
 from MCTS_avgscore import mcts_avg_policy
 from MCTS_maxmoves import mcts_max_policy
 
+def calc_score(scores):
+    score = sum(scores) / len(scores)
+    return score
+
+def calc_tile(tiles):
+    benchmark = [512, 1024, 2048, 4096]
+    res = {}
+    length = len(tiles)
+
+    for item in benchmark:
+        count = 0
+        for tile in tiles:
+            if item == tile:
+                count += 1
+        res[item] = (count / length) * 100
+
+    return res
+
 def display_policy(policy, name, iterations, count=False):
     average_score = []
+    tiles = []
 
     print("--------------------")
     print(name)
@@ -18,18 +37,22 @@ def display_policy(policy, name, iterations, count=False):
         score, max_tile = game.simulate_game(policy, show_board=False, show_score=False) if not count else game.simulate_count_moves(policy, show_board=False, show_score=False)
         print(f"{score} (tile={max_tile})")
         average_score.append(score)
+        tiles.append(max_tile)
 
-    res = sum(average_score) / len(average_score)
-    print(f"\nAverage Score: {res}\n")
+    res_score = calc_score(average_score)
+    res_tile = calc_tile(tiles)
+
+    print(f"\nAverage Score: {res_score}")
+    print(f"Tile Percentage: {res_tile}\n")
 
 if __name__ == "__main__":
-    iterations = 2
+    iterations = 10
     baseline1 = random_policy()
     baseline2 = greedy_policy(game)
     policy1 = mcts_avg_policy(0.05)
     policy2 = mcts_max_policy(0.05)
 
-    display_policy(baseline1, "greedy", iterations)
+    display_policy(baseline1, "random", iterations)
     display_policy(baseline2, "greedy", iterations)
     display_policy(policy1, "mcts average", iterations)
     display_policy(policy2, "mcts max depth", iterations, count=True)
